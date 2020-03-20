9415fe66bdf3572d9046c6da6d3fd5c5931493f7
AliciaMerlino <acmerlino1@gmail.com>
2020-03-20 19:49:29 +0000

    Add popper.js to allow drop down menu to show logout button. Fix logout routing

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
 end
