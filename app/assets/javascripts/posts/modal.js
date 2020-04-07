22b9c21f93f65c5f7931a3b0b6de7e3406844f13
AliciaMerlino <acmerlino1@gmail.com>
2020-04-07 19:57:52 +0100

    Update modal.js as clicking on post was only showing post with id of 1. Changed to an .on(click) event

--- app/assets/javascripts/posts/modal.js
+++ app/assets/javascripts/posts/modal.js
@@ -1,6 +1,6 @@
 $(document).on("turbolinks:load", function() {
   // when a post is clicked, show its full content in a modal window
-  $("body").click(".single-post-card, .single-post-list", function() {
+  $("body").on("click", ".single-post-card, .single-post-list", function() {
     var posted_by = $(this)
       .find(".post-content .posted-by")
       .html();
