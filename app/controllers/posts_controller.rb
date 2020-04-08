7c516369e93ec9d4b11ef462ae1ebd155b0c4ed5
AliciaMerlino <acmerlino1@gmail.com>
2020-04-08 16:05:17 +0100

    Create branch pages for different interests. Use will_paginate gem to show posts over multiple pages.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,20 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
+  def interest
+    post_for_branch(params[:action])
+  end
+
+  private
+
+  def post_for_branch(branch)
+    @categories = Category.where(branch: branch)
+    @posts = get_posts.paginate(page: params[:page])
+  end
+
+  def get_posts
+    Post.limit(30)
+  end
+
 end
