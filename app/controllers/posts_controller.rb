bae1c8e12a48abeedaac8b7e14fdaa46a010505b
AliciaMerlino <acmerlino1@gmail.com>
2020-04-18 15:56:28 +0100

    Infinite scroll added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,10 @@ class PostsController < ApplicationController
   def post_for_branch(branch)
     @categories = Category.where(branch: branch)
     @posts = get_posts.paginate(page: params[:page])
+    respond_to do |format|
+      format.html
+      format.js { render partial: 'posts/posts_pagination_page' }
+    end
   end
 
   def get_posts
@@ -28,7 +32,6 @@ class PostsController < ApplicationController
       posts = Post.by_category(branch, category)
     elsif category.present? && search.present?
       posts = Post.by_category(branch, category).search(search)
-    else
     end
   end
 
