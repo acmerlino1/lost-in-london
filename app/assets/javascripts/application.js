930905b58799a5fac97ffddc75218c86883fdd94
AliciaMerlino <acmerlino1@gmail.com>
2020-04-24 10:36:26 +0100

    New branch to add a React frontend. Clear rails app

--- app/assets/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap/dropdown
-//= require bootstrap-sprockets
-//= require_tree .
--- app/controllers/pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class PagesController < ApplicationController
-
-  def index
-    @posts = Post.limit(5)
-  end
-  
-end
